33a0e3dbaa3d77d6e6670093ae68dd762faa32ad
Unmesh Kanchan <unmeshkanchan25@gmail.com>
2019-03-15 10:14:10 -0700

    Add files via upload

---
+++ ASP_NET_Core_WebAPI/ASP_NET_Core_WebAPI/Models/ApplicationUserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NET_Core_WebAPI.Models
+{
+    public class ApplicationUserModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+    }
+}
